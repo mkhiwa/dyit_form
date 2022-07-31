40951a1e93f2abf8d1ac7e870c0e4306f49c0ab1
alainm21 <alain.mordaunt21@gmail.com>
2022-07-31 23:17:09 +0200

    routes newsletter

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :forms
   resources :newsletters
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  root to: "forms#index"
+  root to: "newsletters#index"
   # Defines the root path route ("/")
   # root "articles#index"
 end
